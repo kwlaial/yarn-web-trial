762f17b30d7275835771088e527af9984ed7892f
kwlaial <85923968+kwlaial@users.noreply.github.com>
2022-08-10 13:04:53 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/4lQ3BvxcYrtnZ6mN/scene.splinecode" />
+    <div className="flex w-screen h-screen min-h-screen flex-col items-center justify-center relative">
+      {/*Navigation Bar*/}
+      <nav className="w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center">
+        
+      </nav>
+      <div className="relative" id="home">
+        <Spline scene="https://prod.spline.design/4lQ3BvxcYrtnZ6mN/scene.splinecode" />
+      </div>
+    </div>
   );
 }
 
