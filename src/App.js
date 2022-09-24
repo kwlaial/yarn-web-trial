e13e9e0c2a9720d648d88e2c18d3de10f8f084d9
kwlaial <85923968+kwlaial@users.noreply.github.com>
2022-09-23 23:05:01 -0400

    start

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <div className="flex w-screen h-screen min-h-screen flex-col items-center justify-center relative">
+    <div className="flex w-screen h-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
       {/*Navigation Bar*/}
-      <nav className="w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center">
-        
+      <nav className="w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center"> 
+        <div className="w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center"></div>
+        <p className="text-lg text-textBase font-medium"> LAI KAM WAI</p>
+
+        <div className="hidden md: flex items-center gap-6 ml-6 flex-1">
+          <a href="#home" className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100
+          ease-in-out">Home</a>
+        </div>
       </nav>
       <div className="relative" id="home">
-        <Spline scene="https://prod.spline.design/4lQ3BvxcYrtnZ6mN/scene.splinecode" />
+        <Spline scene="https://prod.spline.design/jkji4T0ieM4AK59g/scene.splinecode" />
       </div>
     </div>
   );
